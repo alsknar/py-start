549a87dd24181296348c899cf0f3682847bcad57
alsknar <anton.lavrenenko@gmail.com>
2019-01-10 13:52:38 +0200

    add cycle while and for

--- HelloWorld.py
+++ HelloWorld.py
@@ -1,5 +1,14 @@
 print("Hello World!")
 name = input("What is your name: ")
-print("Hello,", name)
+# Переменные можна устанавливать через запятую сколько угодно. Будут выводится через пробел
+print("Hello,", name,name,name)
 
+i = 5
+while i < 15:
+    print(i)
+    i = i + 2
+
+for i in 'hello world':
+    # end - что будет подставлятся в конце строки. По умолчанию \n перенос строки
+    print(i * 2, end='')
 
