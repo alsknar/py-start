07ec48345ccf1b30d1c637e9093c862044e89cfd
alsknar <anton.lavrenenko@gmail.com>
2019-05-20 15:54:25 +0300

    add change in file for.py

--- for.py
+++ for.py
@@ -1,14 +1,17 @@
+i = 0
 while i < 15:
     print(i)
     i = i + 2
 
 for i in 'hello world':
     print(i*2, end='')
+    print('',end='\n')
 
 for i in 'hello world':
     if i == 'o':
         continue
     print(i * 2, end='')
+    print()
 
 for i in 'hello world':
     if i == 'o':
